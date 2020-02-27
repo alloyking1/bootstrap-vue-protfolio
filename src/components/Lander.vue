d786b45e5cead748348a2a0bb555a47236471f5a
alloyking1 <alloyking1@gmail.com>
2020-02-27 07:48:12 -0500

    fixed bug

--- src/components/Lander.vue
+++ src/components/Lander.vue
@@ -83,7 +83,7 @@
             <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                 <div class="modal-dialog modal-lg">
                     <div class="modal-content">
-                        <contact/>
+                        <Contact/>
                     </div>
                 </div>
             </div>
